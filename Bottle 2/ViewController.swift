a13e6458b30f9da4ad8355465e264ce24b3e194f
Vedant Jain <thevedantjain@gmail.com>
2019-04-06 17:08:50 +0530

    Initial Commit

---
+++ Bottle 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bottle 2
+//
+//  Created by Vedant Jain on 06/04/19.
+//  Copyright © 2019 Vedant Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bottle 2Tests/Bottle_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Bottle_2Tests.swift
+//  Bottle 2Tests
+//
+//  Created by Vedant Jain on 06/04/19.
+//  Copyright © 2019 Vedant Jain. All rights reserved.
+//
+
+import XCTest
+@testable import Bottle_2
+
+class Bottle_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
